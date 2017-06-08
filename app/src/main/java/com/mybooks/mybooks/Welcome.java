ef1a36182876cdbe41d4ad2f4ca065fcb2ea1012
amitvrkv <amitrkv@yahoo.in>
2017-06-08 14:21:24 +0530

    test commit

--- app/src/main/java/com/mybooks/mybooks/Welcome.java
+++ app/src/main/java/com/mybooks/mybooks/Welcome.java
@@ -1,6 +1,7 @@
 package com.mybooks.mybooks;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,8 +13,6 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
-
-
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
 }
