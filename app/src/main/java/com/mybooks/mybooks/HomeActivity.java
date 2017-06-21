2877de26bbe1cd345e821a50e4d85507caf218bb
amitvrkv <amitrkv@yahoo.in>
2017-06-21 14:16:51 +0530

    21/06/2017 done with order

--- app/src/main/java/com/mybooks/mybooks/HomeActivity.java
+++ app/src/main/java/com/mybooks/mybooks/HomeActivity.java
@@ -99,7 +99,7 @@ public class HomeActivity extends AppCompatActivity
                 break;
 
             case R.id.customiseOrderMenu:
-                //Toast.makeText(getApplicationContext(), getDate() , Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), CustomOrderActivity.class));
                 break;
 
             case R.id.myCartMenu :
